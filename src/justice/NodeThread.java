165d210d7d0f79c8ab873e5e2cf905c11b15b50d
Ting Yu <tingyu_tan@mymail.sutd.edu.sg>
2018-03-05 17:41:47 +0800

    redirect input and output

--- src/justice/NodeThread.java
+++ src/justice/NodeThread.java
@@ -11,16 +11,7 @@ class NodeThread extends Thread {
 	
 	NodeThread(ProcessGraphNode node) {
 		this.node = node;
-		// augment command to redirect input and output if not default
-		String command = node.getCommand();
-		if (!node.getInputFile().getName().equals("stdin")) {
-			command += " < " + node.getInputFile();
-		}
-		if (!node.getOutputFile().getName().equals("stdout")) {
-			command += " > " + node.getOutputFile();
-		}
-		System.out.println(command);
-		commandList = command.split(" ");
+		commandList = node.getCommand().split(" ");
 		// set runnable to false
 		node.setNotRunnable();
 	}
@@ -29,6 +20,14 @@ class NodeThread extends Thread {
 	public void run() {
 		try {
     		ProcessBuilder pb = new ProcessBuilder(commandList);
+    		// redirect input and output if not default
+    		if (!node.getInputFile().getName().equals("stdin")) {
+    			pb.redirectInput(node.getInputFile());
+    		}
+    		if (!node.getOutputFile().getName().equals("stdout")) {
+    			pb.redirectOutput(node.getOutputFile());
+    		}
+    		
     		// read and display console output
     		BufferedReader br = new BufferedReader(new 
 					InputStreamReader(pb.start().getInputStream()));
@@ -36,6 +35,7 @@ class NodeThread extends Thread {
 				System.out.println(line);
 			}
 			br.close();
+			
 			// set executed to true
 	    	node.setExecuted();
     	} catch (IOException e) {
